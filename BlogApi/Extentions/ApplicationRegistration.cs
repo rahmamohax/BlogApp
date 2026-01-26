41bcf7ca9f27f6fa6065dac1b8d0e089665a2ec7
rahma2005 <rhmarhma360@gmail.com>
2026-01-26 18:51:02 +0200

    add extentions and result class

---
+++ BlogApi/Extentions/ApplicationRegistration.cs
@@ -0,0 +1,37 @@
+﻿using Blog.Domain.Contracts;
+using Blog.Persistence.Data.DataSeed;
+using Blog.Persistence.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApi.Web.Extentions
+{
+    public static class ApplicationRegistration
+    {
+        public static async Task<WebApplication> MigrateDatabase(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<BlogDbContext>();
+            var pending = await db.Database.GetPendingMigrationsAsync();
+            if(pending != null) 
+                await db.Database.MigrateAsync();
+            return app;
+        }
+
+        public static async Task<WebApplication> SeedDatabaseAsync(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+            var seeder = scope.ServiceProvider.GetRequiredService<IDataSeeder>();
+            await seeder.InitializeAsync();
+            return app;
+        }
+
+        public static async Task<WebApplication> SeedIdentityDataAsync(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+            var seeder = scope.ServiceProvider.GetRequiredService<IIdentityDataSeeder>();
+            await seeder.InitializerAsync();
+            return app;
+        }
+
+    }
+}
